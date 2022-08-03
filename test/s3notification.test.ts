d56727a6929e6cdd81b2c44cd8a815b1a022c16f
yomex4life <yomex4life2001@gmail.com>
2022-08-03 08:08:15 +0100

    Initial commit

---
+++ test/s3notification.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S3Notification from '../lib/s3notification-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s3notification-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S3Notification.S3NotificationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
