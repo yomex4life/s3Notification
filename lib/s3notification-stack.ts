ea723f680b0b5dcd3aa785d90379e3db47881b30
yomex4life <yomex4life2001@gmail.com>
2022-08-07 10:54:35 +0100

    initial push

--- lib/s3notification-stack.ts
+++ lib/s3notification-stack.ts
@@ -1,16 +1,52 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Bucket, EventType } from 'aws-cdk-lib/aws-s3';
+import { Runtime, Code, Function } from 'aws-cdk-lib/aws-lambda';
+import { RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { NodejsFunctionProps, NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { join } from 'path';
+import { LambdaDestination } from 'aws-cdk-lib/aws-s3-notifications';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class S3NotificationStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new Bucket(this, 'MyFirstBucket', {
+      bucketName: '4life-cdk-cloudfront-s3',
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'S3NotificationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const fileTable = new Table(this, 'file-table', {
+      partitionKey: {name: 'id', type: AttributeType.STRING},
+      tableName: 'product',
+      removalPolicy: RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST
+    });
+
+    
+    const nodeJsProductFunctionProps: NodejsFunctionProps = {
+      bundling: {
+      externalModules: [
+          'aws-sdk'
+      ]
+      },
+      environment: {
+          PRIMARY_KEY: 'id',
+          DYNAMODB_TABLE_NAME: fileTable.tableName,
+      },
+      runtime: Runtime.NODEJS_16_X
+    } 
+
+    const fileFunction = new NodejsFunction(this, 'file-lambda-function', {
+      entry: join(__dirname, `/../src/product/index.js`),
+      ...nodeJsProductFunctionProps
+    });
+
+    bucket.addEventNotification(EventType.OBJECT_CREATED, new LambdaDestination(fileFunction), {suffix: '.png'});
+    fileTable.grantReadWriteData(fileFunction);
+
   }
 }
